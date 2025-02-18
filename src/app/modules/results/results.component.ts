8785298b398890ce51d7d736be83683764ef03b2
RadoslawNagiel <nagiel.media@gmail.com>
2025-02-18 12:19:07 +0100

    Feat: Add results page component

---
+++ src/app/modules/results/results.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-results',
+  imports: [TranslateModule],
+  templateUrl: './results.component.html',
+  styleUrl: './results.style.scss',
+})
+export default class ResultsComponent {}
