fafaf893dde508fea916684a28198d73aa37a010
RadoslawNagiel <nagiel.media@gmail.com>
2025-02-18 12:18:55 +0100

    Feat: Add quiz page component

---
+++ src/app/modules/quiz/quiz.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+import { TranslateModule } from '@ngx-translate/core';
+
+@Component({
+  selector: 'app-quiz',
+  imports: [TranslateModule],
+  templateUrl: './quiz.component.html',
+  styleUrl: './quiz.style.scss',
+})
+export default class QuizComponent {}
