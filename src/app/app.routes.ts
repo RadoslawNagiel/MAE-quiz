b7522214cda2bca4c04319f4abb7c28818974cb3
RadoslawNagiel <nagiel.media@gmail.com>
2025-02-18 11:36:37 +0100

    chore: update main project structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: ``,
+    loadComponent: async () => import(`./modules/home/home.component`),
+  },
+  {
+    path: `**`,
+    redirectTo: ``,
+  },
+];
