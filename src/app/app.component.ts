94ce9a3ea0f00eff2372fd3e037d817a9a919c8b
RadoslawNagiel <nagiel.media@gmail.com>
2025-02-18 12:51:19 +0100

    Feat: Add home page functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, inject } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { TranslateModule, TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, RouterLink, TranslateModule],
+  templateUrl: `./app.component.html`,
+  styleUrl: `./app.style.scss`,
 })
-export class AppComponent {}
+export class AppComponent {
+  readonly translateService = inject(TranslateService);
+  readonly languages = [
+    {
+      langKey: `pl`,
+      value: `PL`,
+    },
+    {
+      langKey: `en`,
+      value: `EN`,
+    },
+  ];
+
+  setLanguage(language: string) {
+    this.translateService.setDefaultLang(language);
+  }
+}
--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import ButtonComponent from '../../shared/dumb-components/button/button.component';
 
 @Component({
   selector: 'app-home',
-  imports: [TranslateModule],
+  imports: [TranslateModule, ButtonComponent],
   templateUrl: './home.component.html',
+  styleUrl: './home.style.scss',
 })
 export default class HomeComponent {}
