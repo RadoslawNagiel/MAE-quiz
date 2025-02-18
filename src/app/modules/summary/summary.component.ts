8c6edbaf9d6ce89433292178a066e28d69cfdcb6
RadoslawNagiel <nagiel.media@gmail.com>
2025-02-18 14:51:56 +0100

    Feat: Add summary page

--- src/app/modules/summary/summary.component.ts
+++ src/app/modules/summary/summary.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { TranslateModule } from '@ngx-translate/core';
+import { QuizService } from '../../shared/services/quiz.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-summary',
-  imports: [TranslateModule],
+  imports: [TranslateModule, NgClass],
   templateUrl: './summary.component.html',
   styleUrl: './summary.style.scss',
 })
-export default class SummaryComponent {}
+export default class SummaryComponent {
+  readonly quizService = inject(QuizService);
+
+  questions = computed(() => {
+    return this.quizService.questions();
+  });
+}
