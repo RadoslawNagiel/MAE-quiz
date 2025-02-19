a7cfdad260623c55dd9def4c9567c1ca28f87640
RadoslawNagiel <nagiel.media@gmail.com>
2025-02-19 19:03:14 +0100

    Feat: Update summary page

--- src/app/modules/summary/summary.component.ts
+++ src/app/modules/summary/summary.component.ts
@@ -1,18 +1,9 @@
-import { Component, computed, inject } from '@angular/core';
-import { TranslateModule } from '@ngx-translate/core';
-import { QuizService } from '../../shared/services/quiz.service';
-import { NgClass } from '@angular/common';
+import { Component } from '@angular/core';
+import QuizQuestionsComponent from '../../shared/smart-components/quiz-questions/quiz-questions.component';
 
 @Component({
   selector: 'app-summary',
-  imports: [TranslateModule, NgClass],
+  imports: [QuizQuestionsComponent],
   templateUrl: './summary.component.html',
-  styleUrl: './summary.style.scss',
 })
-export default class SummaryComponent {
-  readonly quizService = inject(QuizService);
-
-  questions = computed(() => {
-    return this.quizService.questions();
-  });
-}
+export default class SummaryComponent {}
