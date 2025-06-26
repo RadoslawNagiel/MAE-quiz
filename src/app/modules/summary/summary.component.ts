f244752f7eb1b5eacc05af982ea6141d0718d68e
RadoslawNagiel <nagiel.media@gmail.com>
2025-06-26 18:35:44 +0200

    Chore: add build

--- src/app/modules/summary/summary.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import QuizQuestionsComponent from '../../shared/smart-components/quiz-questions/quiz-questions.component';
-
-@Component({
-  selector: 'app-summary',
-  imports: [QuizQuestionsComponent],
-  templateUrl: './summary.component.html',
-})
-export default class SummaryComponent {}
